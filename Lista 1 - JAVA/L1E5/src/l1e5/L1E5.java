f6815ee281f1b5e697c9b3a2418dadaa953ffad6
OrllandoJr <38482160+OrllandoJr@users.noreply.github.com>
2018-06-19 18:48:41 -0300

    Lista 1 - JAVA

---
+++ Lista 1 - JAVA/L1E5/src/l1e5/L1E5.java
@@ -0,0 +1,15 @@
+package l1e5;
+
+public class L1E5 {
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println(1 + " " + 2 + " " + 3 + " " + 4);
+        System.out.print(" "+ 1 + " " + 2 + " " + 3 + " " + 4 + "\n");
+        System.out.printf("%d %d %d %d\n", 1, 2, 3, 4);
+        
+        
+    }
+    
+}
